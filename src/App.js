c8e1d725b751cf7b2e298a239804c16e09d25707
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-10-24 14:19:45 +0300

    update app.js file

--- src/App.js
+++ src/App.js
@@ -4,5 +4,34 @@ import TodoList from './TodoList';
 import { useState } from 'react';
 
 const App = () => {
-  
-}
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  // const addTodo = () => {
+  //   const [todos, setTodos] = useState([]);
+  //   const [todo, setTodo] = useState("");
+
+    const addTodo = () => {
+      if(todo !== "") {
+        setTodos([...todos, todo]);
+        setTodo("");
+      }
+    };
+
+    const deleteTodo = (text) => {
+      const newTodos = todos.filter((todo) => {
+        return todo !== text;
+      });
+      setTodos(newTodos);
+    };
+
+    return (
+      <div className='App'>
+        <h1>React Todo App</h1>
+        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+        <TodoList list={todos} remove={deleteTodo} />
+      </div>
+    );
+};
+
+export default App;
