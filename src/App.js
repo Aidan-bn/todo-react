8a7de614c81271af95bb00176a5a374139fcd934
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-10-24 12:24:25 +0300

    Lay a box div

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>TO DO APP WITH REACT</h2>
+
+        <div className='input-content'>
+          <input type='text' name='todo' placeholder='Create todo item'></input>
+          <button className='add-button'>Add To List</button>
+        </div>
       </header>
     </div>
   );
